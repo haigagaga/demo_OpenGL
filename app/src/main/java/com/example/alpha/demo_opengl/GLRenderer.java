90b5e86f87791bf53a3d0b3cab73c3983feb7e6e
haigagaga <276410363@qq.com>
2018-09-28 16:23:05 +0800

    OpenGL的变换其实都是通过矩阵相乘来实现的。

--- app/src/main/java/com/example/alpha/demo_opengl/GLRenderer.java
+++ app/src/main/java/com/example/alpha/demo_opengl/GLRenderer.java
@@ -2,15 +2,29 @@ package com.example.alpha.demo_opengl;
 
 import android.opengl.GLSurfaceView;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by gengchunjiang on 2018/9/28.
+ *
+ *
+ *
+ *
+ *      OpenGL的变换其实都是通过矩阵相乘来实现的。
+ *
+ *
+ *
+ *
  */
 
 public class GLRenderer implements GLSurfaceView.Renderer {
 
+    private FloatBuffer mTriangleBuffer;
     private float[] mTriangleArray = {
             0f, 1f, 0f,
             -1f, -1f, 0f,
@@ -19,7 +33,15 @@ public class GLRenderer implements GLSurfaceView.Renderer {
 
     //OpenGL不对堆中数据进行操作，而是在直接内存中操作。 即操作的数据需要保存到NIO的Buffer中。
     public GLRenderer(float[] mTriangleArray) {
-        this.mTriangleArray = mTriangleArray;
+        //先初始化buffer，数组的长度*4，因为一个float占4个字节
+        ByteBuffer bb = ByteBuffer.allocateDirect(mTriangleArray.length * 4);
+        //以本机字节顺序来修改此缓冲区的字节顺序
+        bb.order(ByteOrder.nativeOrder());
+        mTriangleBuffer = bb.asFloatBuffer();
+        //将给定float[]数据从当前位置开始，依次写入此缓冲区
+        mTriangleBuffer.put(mTriangleArray);
+        //设置此缓冲区的位置。如果标记已定义并且大于新的位置，则要丢弃该标记。
+        mTriangleBuffer.position(0);
     }
 
     @Override
